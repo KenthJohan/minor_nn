2279a08b37ed027c2b7fcc22bd2c6103c1b0540b
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2020-03-10 09:59:20 +0100

    misc

--- minor_nn.c
+++ minor_nn.c
@@ -35,6 +35,7 @@ void cw (double mw1[], double const vd1[], double const va0[], unsigned n1, unsi
 		{
 			//w_rc = a_c * d_r
 			//TODO: What is happening here?
+			//mw1 [n0*r + c] -= va0[c] * vd1[r] * learningrate;
 			mw1 [n1*c + r] -= va0[c] * vd1[r] * learningrate;
 		}
 	}
@@ -45,7 +46,7 @@ void cw (double mw1[], double const vd1[], double const va0[], unsigned n1, unsi
 #define L1 2
 #define L2 1
 #define SAMPLECOUNT 4
-#define LEARNINGRATE 0.5
+#define LEARNINGRATE 0.75
 
 static double x[SAMPLECOUNT][L0] =
 {
@@ -71,34 +72,21 @@ int main (int argc, char * argv [])
 	//lin_test_mv_mul_t ();
 	//return 1;
 
-	//Network topology is (L0, L1, L2):
-	//w1 : L0 inputs and L1 outputs:
-	//w2 : L1 inputs and L2 outputs:
-	double w1 [L1*L0];
-	double w2 [L2*L1];
+	double w1 [L1*L0] = {0};
+	double w2 [L2*L1] = {0};
 	double a1 [L1] = {0};
 	double a2 [L2] = {0};
 	double d1 [L1] = {0};
 	double d2 [L2] = {0};
-	//Activated values:
-	//double a [L1+L2] = {0};
-	//double * a1 = a;
-	//double * a2 = a + L1;
-	//Delta for packpropagation:
-	//double d [L1+L2] = {0};
-	//double * d1 = d;
-	//double * d2 = d + L1;
-	//Mean square error:
 	double mse = 0;
 
-	//Init every weight to a random value with seed = currentime:
 	//srand ((unsigned int)time(0));
-	srand ((unsigned int)3);
+	srand ((unsigned int)1);
 	lin_v_f (w1, lin_rnd, L1*L0);
 	lin_v_f (w2, lin_rnd, L2*L1);
 
 	int iterations = 0;
-	double p[10];
+	double p[10] = {0};
 	while (1)
 	{
 		//Train:
@@ -116,7 +104,7 @@ int main (int argc, char * argv [])
 			cw (w2, d2, a1, L2, L1, LEARNINGRATE);
 		}
 
-		//Evaluate:
+
 		printf ("=============Evaluate==========\n");
 		lin_print (w1, L1, L0, "% 3.1f ", "\n");printf ("\n");
 		lin_print (w2, L2, L1, "% 3.1f ", "\n");printf ("\n");
@@ -141,5 +129,5 @@ int main (int argc, char * argv [])
 
 
 
-
+	return 0;
 }
